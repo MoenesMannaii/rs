beae043cc9e0882ad3197a76a2aea88f00556340
MoenesMannaii <moenes.samp55@gmail.com>
2025-07-01 02:05:43 +0100

    First Commit

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -2,9 +2,6 @@ export const structure = (S: any) =>
   S.list()
     .title('Blog')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
       S.divider(),
       ...S.documentTypeListItems().filter(
       ),
